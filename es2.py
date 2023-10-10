15e1e306d65c23e37602b1ad4668b88825fa0945
Stefano Rossi <stefano19.rossi@mail.polimi.it>
2023-10-10 16:35:41 +0200

    chip

--- es2.py
+++ es2.py
@@ -3,6 +3,7 @@ Make a rock-paper-scissors game to play against the computer. The game should as
 a random choice for the computer, compare the two and print the result. The player should be able to play again until
 they choose to stop by writing "quit"."""
 #quack quack quack
+#chip chip chip
 
 # Import the random module
 from random import choice
